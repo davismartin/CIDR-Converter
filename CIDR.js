a686a5f5507d324c2bac47736859222e6951dd44
davismartin <davisshippmartin@gmail.com>
2015-12-10 14:08:32 -0600

    add(CIDR): add js file

---
+++ CIDR.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  function CIDR() {
+    var startArray = [];
+    var endArray = [];
+    var startDecimal = [];
+
+    var service = {
+
+    };
+
+    return service;
+
+    function storeIpsinArray(val) {
+      
+
+    }
+  }
+})();
